08f82e34c6f063a37604bee0bbb36fbe4610004c
Taylor Wong <trwong93@gmail.com>
2017-11-22 18:39:14 -0800

    Added News Feed CSS skeleton

--- frontend/components/form/post_form.jsx
+++ frontend/components/form/post_form.jsx
@@ -10,6 +10,11 @@ class PostForm extends React.Component {
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.eraseText = this.eraseText.bind(this);
+  }
+
+  eraseText() {
+    document.getElementById('post-form-text-area').value = "";
   }
 
   handleChange(e) {
@@ -17,14 +22,18 @@ class PostForm extends React.Component {
   }
 
   handleSubmit(e) {
-    console.log(this.state);
     e.preventDefault();
     this.props.createPost(this.state);
+    document.getElementById('post-form-text-area').value = "";
+  }
+
+  componentWillReceiveProps(newProps) {
+    this.firstName = this.newProps.currentUser.first_name;
   }
 
   render() {
-    let firstName = this.props.currentUser.first_name;
-
+    // let firstName = this.props.currentUser.first_name;
+    console.log(this.firstName);
     return (
       <div className="post-form-container">
         <form
@@ -32,9 +41,10 @@ class PostForm extends React.Component {
           className="post-form"
           >
           <textarea
+            id="post-form-text-area"
             name="body"
             className="post-form-body"
-            placeholder={`What's on your mind, ${firstName}?`}
+            placeholder={`What's on your mind, ${this.firstName}?`}
             onChange={this.handleChange}
           >
           </textarea>
