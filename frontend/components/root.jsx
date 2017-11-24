dcdc9c3edc28763b8c954ecb4c1d4e16f22dc13d
Taylor Wong <trwong93@gmail.com>
2017-11-24 15:51:03 -0800

    Added scroll to top of page on redirect and link functionality

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 import App from './app';
+import ScrollToTop from "./../util/scroll_util";
 
 export default ({ store }) => (
   <Provider store={store}>
     <HashRouter>
-      <App />
+      <ScrollToTop>
+        <App />
+      </ScrollToTop>
     </HashRouter>
   </Provider>
 );
