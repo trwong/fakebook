04eaab9c64e1c959c30d31fdb6f9cdb63679b5d6
Taylor Wong <trwong93@gmail.com>
2017-12-01 15:55:26 -0800

    Resolved issue with rootReducer and logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,8 +1,27 @@
+// import { createStore, applyMiddleware } from 'redux';
+// import thunk from 'redux-thunk';
+// // import logger from 'redux-logger';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+
+// import rootReducer from '../reducers/root';
+
+// const middlewares = [thunk];
+
+// if (process.env.NODE_ENV !== 'production') {
+//   // must use 'require' (import only allowed at top of file)
+//   const { logger } = require('redux-logger');
+//   middlewares.push(logger);
+// }
+
+// export default (preloadedState = {}) => createStore(
+//   rootReducer, preloadedState,
+//   applyMiddleware(thunk, middlewares)
+// );
+// // composeWithDevTools(applyMiddleware(thunk, logger))
+
+
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-// import logger from 'redux-logger';
-import { composeWithDevTools } from 'redux-devtools-extension';
-
 import rootReducer from '../reducers/root';
 
 const middlewares = [thunk];
@@ -13,8 +32,8 @@ if (process.env.NODE_ENV !== 'production') {
   middlewares.push(logger);
 }
 
-export default (preloadedState = {}) => createStore(
-  rootReducer, preloadedState,
-  applyMiddleware(thunk, middlewares)
-  // composeWithDevTools(applyMiddleware(thunk, logger))
+const configureStore = (preloadedState = {}) => (
+  createStore(rootReducer, preloadedState, applyMiddleware(...middlewares))
 );
+
+export default configureStore;
