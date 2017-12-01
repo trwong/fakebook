b2fe06412a0e9ed82d8816d2f41c5b5d42047b04
Taylor Wong <trwong93@gmail.com>
2017-12-01 14:07:07 -0800

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,49 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# # Users
+
+headshot_arr = [
+  "https://res.cloudinary.com/trwong/image/upload/v1512165315/christopher-campbell-28567_mqlwnp.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165316/michael-dam-258165_xozcje.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165315/ryan-holloway-261284_qqmizu.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165320/erik-lucatero-310633_wx4zhf.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165314/lucas-sankey-378674_srcc3x.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165314/alexandru-zdrobau-98438_bbq3fk.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165312/malvestida-magazine-295605_qzddzg.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165313/joe-gardner-149699_qcvzbd.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165313/matheus-ferrero-216385_jeg69t.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165311/els-fattah-371118_zu7owm.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165310/swaraj-tiwari-126808_ejn6xe.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165312/audrey-m-jackson-260657_a8uuag.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165311/jared-sluyter-260692_sgrzcl.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165309/aatik-tasneem-138230_mlanio.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165308/eric-froehling-311481_jn3lz9.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165307/usama-377336_zuzeyl.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165309/joseph-gonzalez-399972_lwyqdh.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165313/donald-teel-349294_ansoh4.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165308/berwin-coroza-340444_n2sni5.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165310/kyu-lee-387624_sw9du5.jpg"
+]
+
+coverpic_arr = [
+  "https://res.cloudinary.com/trwong/image/upload/v1512165736/willian-west-346731_voh9xu.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165738/prometey-sanchez-noskov-340992_acmcgy.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165738/joel-filipe-191372_w0jzo5.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165738/pedro-lastra-157071_zgha8w.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165738/yeshi-kangrang-342857_xh1nxh.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165740/thomas-habr-185321_e37qxn.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165738/pawel-nolbert-291146_knzg8q.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165742/anthony-delanoix-8433_kyrpxb.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165740/eric-carlson-335197_x2iv2r.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165742/sebastian-unrau-31677_yv2smy.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165743/michal-kmet-257136_ct42ep.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165741/irene-davila-329992_fdmjqs.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165743/michal-parzuchowski-40545_kihkkp.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165743/billy-williams-262740_mejlkv.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165749/pietro-de-grandi-329892_rainpv.jpg",
+  "https://res.cloudinary.com/trwong/image/upload/v1512165745/daniel-roe-30147_vowads.jpg"
+]
+
 User.create(first_name: 'Taylor',
   last_name: 'Wong',
   email: "tay@tay.com",
@@ -15,34 +57,10 @@ User.create(first_name: 'Taylor',
   gender: "male",
   profile_img_url: "https://res.cloudinary.com/trwong/image/upload/v1512000606/download_1_ec1e6x.jpg",
   cover_img_url: "https://res.cloudinary.com/trwong/image/upload/v1511898507/zion_national_park_springdale_utah_05_urgaqg.jpg")
-# User.create(first_name: 'Karim', last_name: 'Key', email: "test1", password: "password", birthday: "1993-10-31", gender: "male")
-# User.create(first_name: 'Jahar', last_name: 'Peele', email: "test2", password: "password", birthday: "1993-10-31", gender: "male")
-# User.create(first_name: 'Bob', last_name: 'Belcher', email: "test3", password: "password", birthday: "1993-10-31", gender: "male")
-# User.create(first_name: 'Tina', last_name: 'Belcher', email: "test4", password: "password", birthday: "1993-10-31", gender: "male")
-
-# # Posts
-# Post.create(body: "post 1 to user 5 by Taylor", author_id: 1, recipient_id: 5)
-# Post.create(body: "post 2 to user 4 by Karim", author_id: 2, recipient_id: 4)
-# Post.create(body: "post 3 to user 2 by Jahar", author_id: 3, recipient_id: 2)
-# Post.create(body: "post 4 to user 3 by Bob", author_id: 4, recipient_id: 3)
-# Post.create(body: "post 5 to user 4 by Tina", author_id: 5, recipient_id: 4)
-# Post.create(body: " that is a spicy meatball", author_id: 5, recipient_id: 1)
-# Post.create(body: "hmmmmmmhmmmm so good and tasty", author_id: 5, recipient_id: 1)
-
-# # Comments
-# Comment.create(body: "comment 1 on post 1 by Taylor", author_id: 1, post_id: 1)
-# Comment.create(body: "comment 2 on post 1 by Karim", author_id: 2, post_id: 1)
-# Comment.create(body: "comment 3 on post 1 by Jahar", author_id: 3, post_id: 1)
-# Comment.create(body: "comment 4 on post 1 by Bob", author_id: 4, post_id: 1)
-# Comment.create(body: "comment 5 on post 1 by Tina", author_id: 5, post_id: 1)
-# Comment.create(body: "Engage Docking Vore Protocol", author_id: 4, post_id: 7)
-# Comment.create(body: "Butt as a service", author_id: 3, post_id: 7)
-
-# Users
 genderArr = ["male", "female"]
 
 Faker::UniqueGenerator.clear
-
+i = 0
 20.times do
   faker_name = Faker::GameOfThrones.character
   first_name = faker_name.split.first
@@ -51,7 +69,10 @@ Faker::UniqueGenerator.clear
   password = "password"
   birthday = Faker::Date.birthday(18, 65)
   gender = genderArr[Random.rand(2)]
-  User.create(first_name: first_name, last_name: last_name, email: email, password: password, birthday: birthday, gender: gender)
+  profile_img_url = headshot_arr[i % headshot_arr.length]
+  cover_img_url = coverpic_arr[i % headshot_arr.length]
+  i += 1
+  User.create(first_name: first_name, last_name: last_name, email: email, password: password, birthday: birthday, gender: gender, profile_img_url: profile_img_url, cover_img_url: cover_img_url)
 end
 
 50.times do
